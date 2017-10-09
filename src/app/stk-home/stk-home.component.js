a52f62f8a583934cd303da095302f41c06b385b0
PetrusStarken <pedrothm91@gmail.com>
2017-10-08 23:12:04 -0300

    message for user not found

--- src/app/stk-home/stk-home.component.js
+++ src/app/stk-home/stk-home.component.js
@@ -13,7 +13,12 @@
     vm.searchOnGit = searchOnGit;
 
     function searchOnGit(searchString) {
+      if (!searchString) {
+        return;
+      }
+
       userService.getUsers(searchString).then(function (res) {
+        vm.notFound = (!res.data.total_count);
         vm.usersCount = res.data.total_count;
         vm.users = res.data.items;
       });
