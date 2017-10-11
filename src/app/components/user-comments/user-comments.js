0959c23045c464d73066e2a407d9ebb2a6614f55
PetrusStarken <pedrothm91@gmail.com>
2017-10-11 15:56:36 -0300

    created component to add comments abou a user

---
+++ src/app/components/user-comments/user-comments.js
@@ -0,0 +1,55 @@
+(function (angular) {
+  'use strict';
+
+  angular.module('app').component('stkUserComments', {
+    templateUrl: 'app/components/user-comments/user-comments.html',
+    controller: userCommentsController,
+    bindings: {
+      login: '@userLogin'
+    }
+  });
+
+  function userCommentsController($modal) {
+    var vm = this;
+
+    vm.$onInit = function () {
+
+    };
+
+    vm.openCommentsModal = function (login) {
+      var modalInstance = $modal.open({
+        templateUrl: 'app/components/user-comments/user-comments-modal.html',
+        controller: userCommentsModalController,
+        controllerAs: '$ctrl',
+        resolve: {
+          login: function () {
+            return login;
+          }
+        }
+      });
+    };
+
+    /** @ngIniject */
+    function userCommentsModalController($scope, $modalInstance, login) {
+      var vm = this;
+
+      vm.login = login;
+      vm.comments = [];
+
+      vm.addComment = addComment;
+
+      function addComment(comment) {
+        var commentModel = {
+          id: vm.comments.length,
+          message: comment,
+          login: vm.login,
+          createdDate: new Date()
+        };
+        vm.comments.push(commentModel);
+
+        delete vm.comment;
+      }
+    }
+  }
+
+})(angular);
