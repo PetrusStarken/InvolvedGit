1117abe17909ffd50d0cfaac6558bbd0a34323b6
PetrusStarken <pedrothm91@gmail.com>
2017-10-11 15:55:52 -0300

    little refactoring and added user not found compoment

--- src/app/pages/user-repositories/user-repositories.js
+++ src/app/pages/user-repositories/user-repositories.js
@@ -7,37 +7,45 @@
   });
 
   /** @ngInject */
-  function userDetailsController($stateParams, repositoryService) {
+  function userDetailsController($stateParams, repositoryService, userService) {
     var vm = this;
 
     vm.setUserRepositories = setUserRepositories;
 
     vm.$onInit = function () {
-      vm.showPagination = false;
       vm.userLogin = $stateParams.login;
-      vm.pagination = {
-        page: 1,
-        itemsPerPage: 5,
-        totalItems: 0
-      };
 
-      setUserRepositories(vm.userLogin, vm.pagination);
+      checkIfUserExists(vm.userLogin).then(function (res) {
+        vm.userFound = (res.data.total_count > 0);
+
+        if (!vm.userFound) {
+          return;
+        }
+
+        vm.pagination = {
+          page: 1,
+          itemsPerPage: 5,
+          totalItems: 0
+        };
+
+        setUserRepositories(vm.userLogin, vm.pagination);
+      });
     };
 
     function setUserRepositories(login, pagination) {
       repositoryService.getRepositories(login, pagination)
-        .then(onSuccess)
-        .catch(onError);
+        .then(onSuccess);
 
       function onSuccess(res) {
         vm.pagination.totalItems = res.data.total_count;
         vm.repositories = res.data.items;
         vm.showPagination = true;
       }
+    }
 
-      function onError() {
-        // TODO: catch exception globally
-      }
+    function checkIfUserExists(userLogin) {
+      var emptyPagination = {};
+      return userService.getUsers(userLogin, emptyPagination);
     }
   }
 })(angular);
