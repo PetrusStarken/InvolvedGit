62356fb5671a2a5722201407d9ce79e383d3aa27
PetrusStarken <pedrothm91@gmail.com>
2017-10-12 16:58:33 -0300

    fixes #18

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -2,6 +2,18 @@
   'use strict';
 
   angular.module('app').component('stkHeader', {
-    templateUrl: 'app/components/header/header.html'
+    templateUrl: 'app/components/header/header.html',
+    controller: headerController
   });
+
+  /** @ngInject */
+  function headerController() {
+    var vm = this;
+
+    vm.showHideMobileMenu = showHideMobileMenu;
+
+    function showHideMobileMenu() {
+      vm.showMobileMenu = !vm.showMobileMenu;
+    }
+  }
 })(angular);
